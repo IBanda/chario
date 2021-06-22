c79dd06a3e8df8964049f7c18026b3a458d45e16
Ian <38500123+IBanda@users.noreply.github.com>
2021-06-22 15:20:32 +0800

    add tranfer routes

--- server/routes/wallet.js
+++ server/routes/wallet.js
@@ -3,6 +3,8 @@ import {
   createCompanyWallet,
   createPersonalWallet,
   addWalletFunds,
+  transferwalletFunds,
+  setTransferResponse,
 } from '../controllers/wallet.js';
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.post('/company/create', createCompanyWallet);
 router.post('/personal/create', createPersonalWallet);
 router.post('/account/deposit', addWalletFunds);
+router.post('/account/transfer', transferwalletFunds);
+router.post('/account/transfer/response', setTransferResponse);
 
 export default router;
