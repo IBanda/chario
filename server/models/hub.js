8a9a7203e76dd5d5e3b0de3b4d05a8b648fde1e4
Ian <38500123+IBanda@users.noreply.github.com>
2021-06-26 16:00:51 +0800

    add extra fields

--- server/models/hub.js
+++ server/models/hub.js
@@ -3,7 +3,12 @@ import mongoose from 'mongoose';
 const { Schema, model } = mongoose;
 
 const HubSchema = new Schema({
+  name: String,
   wallet: String,
+  interest_rate: Number,
+  max_deposit: Number,
+  min_deposit: Number,
+  members: [{ type: Schema.Types.ObjectId, ref: 'user' }],
 });
 
 const Hub = model('hub', HubSchema);
