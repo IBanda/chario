d6a503fc402db8841d8827b7bf2c8a07ea74c469
Ian <38500123+IBanda@users.noreply.github.com>
2021-06-26 16:03:18 +0800

    add new controllers

--- server/routes/hub.js
+++ server/routes/hub.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import {
-  createCompanyWallet,
+  createHubWallet,
   createPersonalWallet,
   addWalletFunds,
   transferwalletFunds,
   setTransferResponse,
+  joinHub,
 } from '../controllers/hub.js';
 
 const router = express.Router();
 
-router.post('/company/create', createCompanyWallet);
-router.post('/personal/create', createPersonalWallet);
+router.post('/join', joinHub);
+router.post('/wallet/group/create', createHubWallet);
+router.post('/wallet/personal/create', createPersonalWallet);
 router.post('/account/deposit', addWalletFunds);
 router.post('/account/transfer', transferwalletFunds);
 router.post('/account/transfer/response', setTransferResponse);
