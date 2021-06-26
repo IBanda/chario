43d4ad112b0c3832c6c13d8a3fa6d147bbb621f2
Ian <38500123+IBanda@users.noreply.github.com>
2021-06-26 15:51:07 +0800

    add session middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import logger from 'morgan';
+import session from 'express-session';
 import './db.js';
 
 import hubRouter from './routes/hub.js';
 import userRouter from './routes/user.js';
 import loanRouter from './routes/loan.js';
+import auth from './auth/auth.js';
 
 const app = express();
 
@@ -12,8 +14,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/hub', hubRouter);
+app.use(
+  session({
+    secret: process.env.COOKIE_SESSION,
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+app.use('/hub', auth, hubRouter);
 app.use('/user', userRouter);
-app.use('/loan', loanRouter);
+app.use('/loan', auth, loanRouter);
 
 export default app;
