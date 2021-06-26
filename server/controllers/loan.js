55ad739e36b361e858493c1666761a864cbb10c2
Ian <38500123+IBanda@users.noreply.github.com>
2021-06-27 03:25:40 +0800

    transfer funds on approved

--- server/controllers/loan.js
+++ server/controllers/loan.js
@@ -1,11 +1,14 @@
 /* eslint-disable consistent-return */
 import Hub from '../models/hub.js';
 import Loan from '../models/loan.js';
-import transporter from '../utils/transporter.js';
+import sendMail from '../utils/transporter.js';
+import fetch from 'node-fetch';
+import getHeaders from '../utils/getHeaders.js';
+import getSignature from '../utils/getSignature.js';
 
 export async function requestLoan(req, res, next) {
-  let i = 0;
   const { loaner, ...body } = req.body;
+
   try {
     const hasPending = await Loan.exists({ status: 'pending' });
     if (hasPending) {
@@ -15,25 +18,26 @@ export async function requestLoan(req, res, next) {
         message: "There's a request waiting for action",
       });
     }
-
-    const user = req.session.user;
-    const request = await Loan.create({ ...body, loaner, loanee: user });
+    const request = await Loan.create({
+      ...body,
+      loaner,
+      loanee: req.session.user,
+    });
     const { members } = await Hub.findById(loaner, 'members').populate({
       path: 'members',
     });
 
-    while (i < members.length) {
-      // eslint-disable-next-line no-await-in-loop
-      await transporter.sendMail({
-        from: `Chario <chario@ianbanda.com>`,
-        to: members[i].email,
+    const notifyMembers = members.map((member) =>
+      sendMail({
+        to: member.email,
         subject: 'Loan Request',
         text: 'Hello',
         html: '<h1>Hello</h1>',
-      });
-      // eslint-disable-next-line no-plusplus
-      i++;
-    }
+      })
+    );
+
+    // eslint-disable-next-line no-undef
+    await Promise.all(notifyMembers);
 
     res.status(201);
     res.json(request);
@@ -42,14 +46,20 @@ export async function requestLoan(req, res, next) {
   }
 }
 
+/*
+Act on the loan request currently pending
+*/
+
 export async function loanAction(req, res, next) {
-  const { status, loanId, hubId, userId, reason } = req.body;
+  const { status, loanId, hubId, userId, reason, wallet } = req.body;
   try {
     if (status !== 'decline' && status !== 'approve') {
-      throw new Error('Status must either be decline or approve');
+      throw new Error(`Status must either be 'decline' or 'approve'`);
     }
 
-    const request = await Loan.findById({ _id: loanId });
+    const request = await Loan.findById({ _id: loanId })
+      .populate('loaner')
+      .populate('loanee');
 
     if (request.approvals.includes(userId) || request.status === 'declined') {
       return res.end();
@@ -60,6 +70,14 @@ export async function loanAction(req, res, next) {
         status: 'declined',
         reason,
       });
+
+      await sendMail({
+        to: request.loanee.email,
+        subject: 'Your loan request has been declined',
+        text: `Your loan request from hub ${request.loaner.name} has been declined`,
+        html: `<p>Your loan request from hub ${request.loaner.name} has been declined</p>`,
+      });
+      // TODO: add message
       res.status(200);
       return res.json({
         status: 'SUCCESS',
@@ -78,10 +96,40 @@ export async function loanAction(req, res, next) {
         status: 'approved',
         active: true,
       });
-      // DISBURSE
+
+      // Transfer funds to loanee wallet
+
+      const body = {
+        amount: request.amount,
+        currency: request.currency,
+        source_wallet: request.loaner.wallet,
+        destination_wallet: wallet,
+      };
+
+      const { signature, salt, timestamp } = getSignature(
+        '/v1/account/transfer',
+        'post',
+        body
+      );
+
+      await fetch('https://sandboxapi.rapyd.net/v1/account/transfer', {
+        method: 'POST',
+        headers: getHeaders(signature, salt, timestamp),
+        body: JSON.stringify(body),
+      });
+
+      await sendMail({
+        to: request.loanee.email,
+        subject: 'Your loan request has been Approved',
+        text: 'Your loan request has been successfully approved, funds have been transfered to your wallet',
+        html: '<p>Your loan request has been successfully approved, funds have been transfered to your wallet</p>',
+      });
     }
+
+    // TODO: add message
+
     res.status(200);
-    return res.json({
+    res.json({
       status: 'SUCCESS',
       message: '',
     });
