f1c6040124d955b30d1f10263a25830abdab11c5
Ian <38500123+IBanda@users.noreply.github.com>
2021-07-04 21:36:47 +0800

    add css

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,10 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    container: {
+      center: true,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
