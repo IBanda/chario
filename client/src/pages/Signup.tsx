3a8a84dd13327e82fc996f99e36db902e92187bb
Ian <38500123+IBanda@users.noreply.github.com>
2021-07-05 00:09:37 +0800

    include opacity & add subheading

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -3,14 +3,17 @@ import SignupForm from 'src/components/SignupForm';
 export default function Signup() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 h-full">
-      <div className="col-span-3 flex flex-col items-center justify-center p-3 bg-gray-900 ">
-        <h1 className="font-bold text-2xl text-blue-500 tracking-tighter mb-4">
+      <div className="col-span-3 flex flex-col items-center justify-center p-3  ">
+        <h1 className="font-bold text-2xl text-blue-500 tracking-tighter">
           Chario
         </h1>
+        <h2 className="font-medium text-sm text-blue-500 tracking-tighter mb-4">
+          Banking with your peers
+        </h2>
         <SignupForm />
       </div>
       <div
-        className="col-span-2 relative  chario__sign-right bg-opacity-50"
+        className="col-span-2 relative  chario__sign-right bg-opacity-100"
         style={{ backgroundImage: 'url(/bg.jpg)' }}
       >
         <div className="max-w-md w-full absolute top-1/2 -left-20 transform -translate-y-1/2 bg-white chario__custom-shadow-blue rounded overflow-hidden ">
