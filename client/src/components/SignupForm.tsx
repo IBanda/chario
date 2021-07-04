27773a3d7155d829d22e4958d927bb80a1c9afd1
Ian <38500123+IBanda@users.noreply.github.com>
2021-07-05 00:08:18 +0800

    extract shareable components

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -1,67 +1,56 @@
 import { useState } from 'react';
+import EmailInput from './EmailInput';
+import AuthInput from './AuthInput';
 
 export default function SignupForm() {
-  const [name, setName] = useState('');
+  const [fname, setFname] = useState('');
+  const [lname, setLname] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmpswd, setConfirmpswd] = useState('');
+
+  const isMissMatch =
+    password.trim() && confirmpswd.trim() && password !== confirmpswd;
+
   return (
     <form className="flex flex-col max-w-sm md:w-96 w-full  p-2  ">
-      <label
-        htmlFor="name"
-        className="text-gray-500 text-sm font-medium tracking-tighter mb-2"
-      >
-        Name
-      </label>
-      <input
-        className="border border-gray-50 shadow rounded p-3 focus:outline-none focus:ring mb-2"
-        type="text"
-        id="name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
-      <label
-        htmlFor="email"
-        className="text-gray-500 text-sm font-medium tracking-tighter mb-2"
-      >
-        Email
-      </label>
-      <input
-        className="border border-gray-50 shadow rounded p-3 focus:outline-none focus:ring mb-2"
-        type="email"
-        id="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
-      <label
-        htmlFor="password"
-        className="text-gray-500 text-sm font-medium tracking-tighter mb-2"
-      >
-        Password
-      </label>
-      <input
-        className="border border-gray-50 shadow rounded p-3 focus:outline-none focus:ring mb-2"
-        type="password"
+      <EmailInput email={email} setEmail={setEmail} />
+      <div className="grid grid-cols-2 gap-4 flex flex-col">
+        <span>
+          <AuthInput
+            id="fname"
+            value={fname}
+            setValue={setFname}
+            label={'First Name'}
+            className="w-full"
+          />
+        </span>
+        <span>
+          <AuthInput
+            id="lname"
+            value={lname}
+            setValue={setLname}
+            label={'Last Name'}
+            className="w-full"
+          />
+        </span>
+      </div>
+      <AuthInput
         id="password"
+        type="password"
         value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
+        setValue={setPassword}
+        label="Password"
+        autoComplete="new-password"
       />
-      <label
-        htmlFor="confirmpswd"
-        className="text-gray-500 text-sm font-medium tracking-tighter mb-2"
-      >
-        Confirm Password
-      </label>
-      <input
-        className="border border-gray-50 shadow rounded p-3 focus:outline-none focus:ring mb-2"
+      <AuthInput
+        className={`${isMissMatch ? 'ring ring-red-500 ring-opacity-40' : ''} `}
         type="password"
         id="confirmpswd"
         value={confirmpswd}
-        onChange={(e) => setConfirmpswd(e.target.value)}
-        required
+        label="Confirm Password"
+        setValue={setConfirmpswd}
+        autoComplete="new-password"
       />
       <span className="text-sm text-gray-500">
         By proceeding, you agree to the{' '}
@@ -70,6 +59,7 @@ export default function SignupForm() {
       <button
         className="text-white font-medium text-lg tracking-tighter bg-blue-500 rounded chario__custom-shadow-blue p-4 mt-4 focus:ring"
         type="submit"
+        disabled={Boolean(isMissMatch)}
       >
         Sign up
       </button>
